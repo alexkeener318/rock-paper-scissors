6c401cb00e03a5bf74a606443c95aa3298b0f177
Alex Keener <alexkeener318@gmail.com>
2022-06-06 11:05:13 -0500

    Implements function that plays one round of the game

--- script.js
+++ script.js
@@ -12,3 +12,38 @@ function computerPlay() {
     }
 }
 
+function playRound(playerSelection, computerSelection){
+    if(computerSelection === "Paper"){
+        if(playerSelection.toLower() === "rock"){
+            return "The computer wins!";
+        }
+        else if(playerSelection.toLower === "paper"){
+            return "Its a tie!";
+        }
+        else{
+            return "The player wins!";
+        }
+    }
+    else if(computerSelection === "Rock"){
+        if(playerSelection.toLower() === "rock"){
+            return "Its a tie!";
+        }
+        else if(playerSelection.toLower === "paper"){
+            return "The player wins!";
+        }
+        else{
+            return "The computer wins!";
+        }
+    }
+    else{
+        if(playerSelection.toLower() === "rock"){
+            return "The player wins!";
+        }
+        else if(playerSelection.toLower === "paper"){
+            return "The computer wins!";
+        }
+        else{
+            return "It's a tie!";
+        }
+    }
+}
